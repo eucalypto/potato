535edbe55dd285b4d32a41fea2834c5dbb12cce8
Viktor Papara <eucalyptov@gmail.com>
2019-06-10 20:23:44 +0200

    Make pythonic loop example PEP8 compliant

--- python/pythonic loops/pythonic-loops.py
+++ python/pythonic loops/pythonic-loops.py
@@ -2,20 +2,20 @@
 
 # this is a non-pythonic loop (a c-like loop):
 
-my_items = ["bacon", "eggs", "sausage",]
+my_items = ["bacon", "eggs", "sausage", ]
 
 i = 0
 while i < len(my_items):
     print(my_items[i])
     i += 1
 
-# The first non-pythonism is: the code manages the iteration variable 
+# The first non-pythonism is: the code manages the iteration variable
 # manually.
-# 
+#
 # A better solution would be to use the range() builtin. Range() gives
 # not a list but a faster immutable iterative object.
 
-my_items = ["bacon", "eggs", "sausage",]
+my_items = ["bacon", "eggs", "sausage", ]
 
 for i in range(len(my_items)):
     print(my_items[i])
@@ -23,15 +23,16 @@ for i in range(len(my_items)):
 # This is still not pythonic since python can iterate over the elements
 # themselves in the most pythonic way:
 
-my_items = ["bacon", "eggs", "sausage",]
+my_items = ["bacon", "eggs", "sausage", ]
 
 for item in my_items:
     print(item)
 
 
-# But what if you need to have an index also? Python can do that! With enumerate().
+# But what if you need to have an index also? Python can do that! With
+# enumerate().
 
-my_items = ["bacon", "eggs", "sausage",]
+my_items = ["bacon", "eggs", "sausage", ]
 
 for i, item in enumerate(my_items):
     print(i, ": ", item)
@@ -44,6 +45,5 @@ breakfast_wishes = {
     "Eve": "just coffee",
 }
 
-for name, breakfast in breakfast_wishes.items(): 
-    print(name, "wants to have", breakfast, "for breakfast." )
-
+for name, breakfast in breakfast_wishes.items():
+    print(name, "wants to have", breakfast, "for breakfast.")
