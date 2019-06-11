499b4cae55752ec42fd552064e8589f588490559
Viktor Papara <eucalyptov@gmail.com>
2019-06-11 19:24:07 +0200

    Add numbers example in special method

--- technologies/java/OCA SE8/1 Java Building Blocks/particles/Particles.java
+++ technologies/java/OCA SE8/1 Java Building Blocks/particles/Particles.java
@@ -16,6 +16,7 @@ public class Particles
         Particles particle = new Particles();
         System.out.println("The mass is:");
         System.out.println(particle.mass);
+        particle.numbers();
     }
     public void collide(){
     }
@@ -23,5 +24,30 @@ public class Particles
     public void propagate()
     {
         System.out.println("Propagate!");
+    }
+
+    public void numbers()
+    {
+        boolean truthiness = false; // works!
+        byte bite_size = 127; // works
+        // byte bite_size1 = 128; // possible lossy conversion from int to byte
+        int normal_size = 12; // works
+        // normal_size = 12L; // possible lossy conversion from long to int
+        long long_size = 13; // works
+        long_size = 12344L;  // works
+
+        // float goat = 123.0;  // possible lossy conversion from double to float
+        float goat = 123.0f; // works
+        double mass = 134_038_341.34; // we can put underscores (_) in between digits of numbers (added in Java7)
+
+        System.out.println(37); // 37
+        System.out.println(0b101); // 5
+        // System.out.println(018); // error: integer number too large. Integers starting with "0" are viewed in octal system
+        System.out.println(017); // 15
+        System.out.println(0x17); // 23
+
+
+
+
     }
 }
