52d96279498e790d385b22292afd1d1532d362e4
Viktor Papara <eucalyptov@gmail.com>
2019-06-11 19:02:38 +0200

    Add class variable and use it from the same class

--- technologies/java/OCA SE8/1 Java Building Blocks/particles/Particles.java
+++ technologies/java/OCA SE8/1 Java Building Blocks/particles/Particles.java
@@ -1,6 +1,8 @@
 package particles;
 public class Particles
 {
+    double mass = 0;
+
     public Particles()
     {
         System.out.println("This is the construction of class 'Particles'");
@@ -11,6 +13,9 @@ public class Particles
         java.util.Random r = new java.util.Random();
         System.out.println(r.nextInt(10));
         System.out.println("This is Class Particles");
+        Particles particle = new Particles();
+        System.out.println("The mass is:");
+        System.out.println(particle.mass);
     }
     public void collide(){
     }
