a1fb5fe2d9fe8adacd875cefe22edb92df6445ae
Viktor Papara <eucalyptov@gmail.com>
2019-06-11 18:12:22 +0200

    Add java test packages

---
+++ technologies/java/OCA SE8/1 Java Building Blocks/fields/Fields.java
@@ -0,0 +1,8 @@
+package fields;
+public class Fields
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, this is the Class Fields");
+    }
+}
---
+++ technologies/java/OCA SE8/1 Java Building Blocks/particles/Particles.java
@@ -0,0 +1,12 @@
+package particles;
+public class Particles
+{
+    public static void main(String[] args)
+    {
+        java.util.Random r = new java.util.Random();
+        System.out.println(r.nextInt(10));
+        System.out.println("This is Class Particles");
+    }
+    public void collide(){
+    }
+}
