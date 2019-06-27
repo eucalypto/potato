14d8cc6b83c949f44bf1cbdb88b631fe2e288e73
Viktor Papara <eucalyptov@gmail.com>
2019-06-27 16:34:57 +0200

    Add ArrayList example in Java

--- technologies/java/OCA SE8/3 Core Java APIs/ArrayGun.java
+++ technologies/java/OCA SE8/3 Core Java APIs/ArrayGun.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 class ArrayGun
 {
     public static void main(String[] args)
     {
         ArrayGun agun = new ArrayGun();
         agun.declarations();
+        agun.arrayListEnumerationCollection();
     }
 
     public void declarations()
@@ -20,5 +23,27 @@ class ArrayGun
         // array3 = 4; // error: incompatible types: int cannot be converted to int[]
         array3 = new int[] {4,1};
         array4 = 4; // bamboozle! array4 is an int!
+    }
+
+    public void arrayListEnumerationCollection()
+    {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("there.");
+        list.add(String.valueOf(Boolean.TRUE));
+        System.out.println(list);
+
+
+        ArrayList list2 = new ArrayList();
+        list2.add("Hello");
+        list2.add("there.");
+        list2.add(String.valueOf(Boolean.TRUE)); // works fine
+        list2.add(Boolean.TRUE); // this is allowed because list2 is a "List" object storing references to all kinds of objects.
+        list2.add(true); // funnily, this works as well
+        // The two lines above give a compiler note:
+        // Note: ArrayGun.java uses unchecked or unsafe operations.
+        // Note: Recompile with -Xlint:unchecked for details.
+
+        System.out.println(list2);
     }
 }
