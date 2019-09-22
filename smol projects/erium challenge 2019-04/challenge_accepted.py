57fd2fb1e3b10e5d50beaec7cfe356db5f65d02f
Viktor Papara <eucalyptov@gmail.com>
2019-09-22 22:07:52 +0200

    Change prime generating function to use numpy array

--- smol projects/erium challenge 2019-04/challenge_accepted.py
+++ smol projects/erium challenge 2019-04/challenge_accepted.py
@@ -1,6 +1,7 @@
 # /usr/bin/env python
 
 import math
+import numpy as np
 
 
 def e_faculty_series(m: int):
@@ -14,17 +15,17 @@ series that defines e. """
 
 def prime_gen(maxdigits: int):
     maxnumber = 10 ** maxdigits
-    candidates = list(range(0, maxnumber))
+    candidates = np.array(range(0, maxnumber))
     for remove_multiples_of in range(2, int(maxnumber / 2)):
         index = 2 * remove_multiples_of
         while index < maxnumber:
-            candidates[index] = None
+            candidates[index] = 0
             index += remove_multiples_of
 
-    return [number for number in candidates if number is not None]
+    return [number for number in candidates if number != 0]
 
 
 if __name__ == '__main__':
-    primes = prime_gen(8)
+    primes = prime_gen(7)
     print(primes[-10:])
     # [99999787, 99999821, 99999827, 99999839, 99999847, 99999931, 99999941, 99999959, 99999971, 99999989]
